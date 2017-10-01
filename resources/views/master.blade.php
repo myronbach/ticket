31edd303dd91d1474fb0ec6d99414a8dafadfa90
Myron <myronbach@gmail.com>
2017-10-01 17:13:35 +0300

    version 1.1

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,14 +9,22 @@
     <title> @yield('title') </title>
 
     <!-- Material Design fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+
+
+
+
+
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- Bootstrap Material Design -->
     <link rel="stylesheet" type="text/css" href="/css/bootstrap-material-design.css">
     <link rel="stylesheet" type="text/css" href="/css/ripples.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/user.css">
 
 
 </head>
